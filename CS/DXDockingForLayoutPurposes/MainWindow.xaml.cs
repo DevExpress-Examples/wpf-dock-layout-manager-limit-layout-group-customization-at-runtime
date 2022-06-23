a977d50915958555f25d1c72fb684f627472889c
Anthony Sam <anton.sameishchev@devexpress.com>
2022-06-23 23:43:12 +0400

    Update MainWindow.xaml.cs

--- CS/DXDockingForLayoutPurposes/MainWindow.xaml.cs
+++ CS/DXDockingForLayoutPurposes/MainWindow.xaml.cs
@@ -1,31 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Markup;
 using DevExpress.Xpf.Core;
-using DevExpress.Xpf.Ribbon;
-using DevExpress.Xpf.Layout.Core;
-using DevExpress.Xpf.Docking;
-using DevExpress.Xpf.Docking.Base;
-using DevExpress.Xpf.Docking.VisualElements;
-using DevExpress.Xpf.Grid;
-using DevExpress.Xpf.Core.Native;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
 
-namespace DXDockingForLayoutPurposes {
+namespace DXDockingForLayoutPurposes
+{
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : DXRibbonWindow {
+    public partial class MainWindow : ThemedWindow {
         public MainWindow() {
             InitializeComponent();
             DataContext = new DataSource();
